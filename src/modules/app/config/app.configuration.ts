1d0e4cc7f27e33958596f2bcf9ad10948f5dde38
Lucas Samuel <lucasskluser@outlook.com>
2021-08-06 21:30:35 -0300

    fix: load envs when yaml not found

--- src/modules/app/config/app.configuration.ts
+++ src/modules/app/config/app.configuration.ts
@@ -1,6 +1,7 @@
 import { readFileSync, existsSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
+import { config as loadEnvVars } from 'dotenv';
 
 const YAML_CONFIG_FILENAME = 'config.yaml';
 
@@ -8,6 +9,7 @@ export default () => {
   const filePath = join(__dirname, YAML_CONFIG_FILENAME);
 
   if (!existsSync(filePath)) {
+    loadEnvVars();
     return;
   }
 
