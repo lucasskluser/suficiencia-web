80e982b4ef01985399dc786ef0ad372d7d3e86a7
Lucas Samuel <lucasskluser@outlook.com>
2021-08-06 21:18:35 -0300

    ci: prepare for heroku

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './modules/app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/modules/app/config/app.configuration.ts
+++ src/modules/app/config/app.configuration.ts
@@ -1,11 +1,15 @@
-import { readFileSync } from 'fs';
+import { readFileSync, existsSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
 
 const YAML_CONFIG_FILENAME = 'config.yaml';
 
 export default () => {
-  return yaml.load(
-    readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'),
-  ) as Record<string, any>;
+  const filePath = join(__dirname, YAML_CONFIG_FILENAME);
+
+  if (!existsSync(filePath)) {
+    return;
+  }
+
+  return yaml.load(readFileSync(filePath, 'utf8')) as Record<string, any>;
 };
